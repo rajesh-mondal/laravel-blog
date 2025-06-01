31d533e7112f031ca9f05c1704e11b079ae02866
Rajesh Mondal <rajesh01749@gmail.com>
2025-06-01 22:21:53 +0600

    render blog posts dynamically from database

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model {
     use HasFactory;
     protected $fillable = ['title', 'slug', 'thumbnail', 'body', 'active', 'published_at', 'user_id'];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
 
     public function user() {
         return $this->belongsTo( User::class );
@@ -15,5 +20,13 @@ class Post extends Model {
 
     public function categories() {
         return $this->belongsToMany( Category::class );
+    }
+
+    public function shortBody( $words = 30 ): string {
+        return Str::words( strip_tags( $this->body ), $words );
+    }
+
+    public function getFormattedDate() {
+        return $this->published_at->format( 'F jS Y' );
     }
 }
