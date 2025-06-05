f92d51a1d45076f94187d6f3673a939933708261
Rajesh Mondal <rajesh01749@gmail.com>
2025-06-05 21:31:04 +0600

    fix: resolve slug conflict between post and category routes

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get( '/', [PostController::class, 'index'] )->name( 'home' );
 Route::get( '/about-us', [SiteController::class, 'about'] )->name( 'about-us' );
-Route::get( '/{category:slug}', [PostController::class, 'byCategory'] )->name( 'by-category' );
+Route::get( '/category/{category:slug}', [PostController::class, 'byCategory'] )->name( 'by-category' );
 Route::get( '/{post:slug}', [PostController::class, 'show'] )->name( 'view' );
